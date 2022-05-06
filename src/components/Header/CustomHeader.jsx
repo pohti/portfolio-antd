14e32a1b58b5c10b9a869db406cd71b515b289e6
Min Marn Oo <min.marnoo@lucence.com>
2022-05-06 15:12:33 +0800

    overriding header colors

--- src/components/Header/CustomHeader.jsx
+++ src/components/Header/CustomHeader.jsx
@@ -1,37 +1,39 @@
 import React, { Component } from 'react'
-import { Row, Col, Typography } from 'antd'
+import { Row, Col, Typography, Menu } from 'antd'
 import { HomeOutlined } from '@ant-design/icons'
 
 import './CustomHeader.css'
 
 const { Text } = Typography
-
-
+const { Item } = Menu
 
 export default class CustomHeader extends Component {
-  render() {
-    return (
-      <div>
-          <Row className="header-row">
-              <Col span={8}>
-                <Row gutter={16} className="header-icon-container">
-                    <Col >
-                        <HomeOutlined className="header-icon"/>
+    render() {
+        return (
+            <div>
+                <Row className="header-row">
+                    <Col span={8}>
+                        <Row gutter={16} className="header-icon-container">
+                            <Col >
+                                <HomeOutlined className="header-icon" />
+                            </Col>
+                            <Col>
+                                <Text className="header-icon-text">Username</Text>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col>
-                        <Text className="header-icon-text">Username</Text>
+                    <Col span={16}>
+                        <Menu
+                            mode="horizontal"
+                            className="header-menu"
+                        >
+                            <Item>Item 1</Item>
+                            <Item>Item 2</Item>
+                            <Item>Item 3</Item>
+                        </Menu>
                     </Col>
                 </Row>
-              </Col>
-              <Col span={16}>
-                  <Row justify='end' gutter={20}>
-                      <Col>Menu 1</Col>
-                      <Col>Menu 2</Col>
-                      <Col>Menu 3</Col>
-                  </Row>
-              </Col>
-          </Row>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
