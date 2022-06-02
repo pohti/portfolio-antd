583e464a922effecdf4456b94055b1b5a538d4d7
Min Marn Oo <min.marnoo@lucence.com>
2022-06-02 15:30:37 +0800

    working on skills page

--- src/components/Content/Skills/Skills.jsx
+++ src/components/Content/Skills/Skills.jsx
@@ -1,9 +1,122 @@
 import React, { Component } from 'react'
+import { Row, Col, Progress, Image, Tabs } from 'antd'
+import {
+  BgColorsOutlined,
+  DatabaseOutlined,
+  MenuOutlined
+} from '@ant-design/icons'
+import {
+  front_end_skills,
+  back_end_skills,
+  other_skills
+} from './skills_data'
+
+import './Skills.css'
+
+const { TabPane } = Tabs
 
 export default class Skills extends Component {
   render() {
     return (
-      <div>Skills</div>
+      <div className="main-skills-div">
+        <Tabs defaultActiveKey='others'>
+          <TabPane
+            tab={<TabTitle icon={<BgColorsOutlined />} title="Front-end" />}
+            key="frontend"
+          >
+            <SkillList data={front_end_skills} />
+          </TabPane>
+
+          <TabPane
+            tab={<TabTitle icon={<DatabaseOutlined />} title="Back-end" />}
+            key="backend"
+          >
+            <SkillList data={back_end_skills} />
+          </TabPane>
+
+          <TabPane
+            tab={<TabTitle icon={<MenuOutlined />} title="Other Skills" />}
+            key="others"
+          >
+            <OtherSkills data={other_skills} />
+          </TabPane>
+        </Tabs>
+      </div>
     )
   }
+}
+
+const TabTitle = ({ icon, title }) => (
+  <span>
+    {icon}{title}
+  </span>
+)
+
+const SkillList = ({ data }) => {
+  const skill_list = data.map(({ skill_name, icon, rating, color }) => (
+
+    <Row className="skill-and-progress-div" key={skill_name}>
+      <Col span={24}>
+        <div className="skill-label-div">
+          {skill_name}
+        </div>
+      </Col>
+      <Col span={24}>
+        <Progress
+          type="line"
+          percent={rating}
+          showInfo={false}
+          strokeWidth={4}
+          strokeLinecap="square"
+          strokeColor={color ? color : "rgb(231, 119, 67)"}
+        />
+      </Col>
+    </Row>
+  ))
+  return (
+    <div>
+      {skill_list}
+    </div>
+  )
+}
+
+const OtherSkills = ({ data }) => {
+
+  if (!data) return null
+
+  const IconList = data.map(({ skill_name, icon_url }) => {
+    return (
+      <Col span={6} className="other-skills-individual-container" key={skill_name}>
+        {/* <Row gutter={[4, 4]}>
+          <Col span={24}>
+            <Image
+              src={icon_url}
+              preview={false}
+              className="other-skills-icon"
+            />
+          </Col>
+          <Col style={{ fontSize: "15px" }} span={24}>
+            <span>{skill_name}</span>
+          </Col>
+        </Row> */}
+
+        <div className="other-skills-icon-container">
+          <Image
+            src={icon_url}
+            preview={false}
+            className="other-skills-icon"
+          />
+        </div>
+        <div className="other-skills-label">
+          <span>{skill_name}</span>
+        </div>
+      </Col>
+    )
+  })
+
+  return (
+    <Row gutter={[4, 16]} className="other-skills-main-div">
+      {data && IconList}
+    </Row>
+  )
 }
---
+++ src/components/Content/Skills/skills_data.js
@@ -0,0 +1,112 @@
+
+export const front_end_skills = [
+  {
+    skill_name: 'React',
+    icon: null,
+    rating: 90,
+  },
+  {
+    skill_name: 'Material UI, Ant Design and Bootstrap',
+    icon: null,
+    rating: 85,
+  },
+  {
+    skill_name: 'Mobx',
+    icon: null,
+    rating: 80,
+  },
+  {
+    skill_name: 'JavaScript',
+    icon: null,
+    rating: 90,
+  },
+  {
+    skill_name: 'HTML',
+    icon: null,
+    rating: 90,
+  },
+  {
+    skill_name: 'CSS',
+    icon: null,
+    rating: 75,
+  },
+]
+
+export const back_end_skills = [
+  {
+    skill_name: 'Python',
+    icon: null,
+    rating: 70,
+  },
+  {
+    skill_name: 'Node.JS',
+    icon: null,
+    rating: 65,
+  },
+  {
+    skill_name: 'Java',
+    icon: null,
+    rating: 65,
+  },
+  {
+    skill_name: 'DynamoDB',
+    icon: null,
+    rating: 75,
+  },
+  {
+    skill_name: 'NoSQL',
+    icon: null,
+    rating: 65,
+  },
+  {
+    skill_name: 'SQL and RDBMS',
+    icon: null,
+    rating: 65,
+  },
+  // {
+  //   skill_name: 'AWS Lambda',
+  //   icon: null,
+  //   rating: 85,
+  // },
+  // {
+  //   skill_name: 'AWS API Gateway',
+  //   icon: null,
+  //   rating: 85,
+  // },
+  // {
+  //   skill_name: 'AWS S3',
+  //   icon: null,
+  //   rating: 85,
+  // },
+]
+
+export const other_skills = [
+  {
+    skill_name: 'Lambda',
+    icon_url: 'https://www.gliffy.com/sites/default/files/image/2020-06/AWS-Lambda_dark-bg.png',
+  },
+  {
+    skill_name: 'API Gateway',
+    icon_url: 'https://awsvideocatalog.com/images/aws/png/PNG%20Light/Networking%20&%20Content%20Delivery/Amazon-API-Gateway.png',
+  },
+  {
+    skill_name: 'S3',
+    icon_url: 'https://res.cloudinary.com/practicaldev/image/fetch/s--bZ7myZIR--/c_imagga_scale,f_auto,fl_progressive,h_1080,q_auto,w_1080/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ip7ork2m951siwgpkety.png',
+  },
+  {
+    skill_name: 'Microsoft Azure',
+    icon_url: 'https://hostingjournalist.com/wp-content/uploads/2017/10/microsoft-azure-cloud-1-300x300.png',
+  },
+  {
+    skill_name: 'Docker',
+    icon_url: 'https://cdn-icons-png.flaticon.com/512/919/919853.png',
+  },
+  {
+    skill_name: 'Git',
+    icon_url: 'https://iconape.com/wp-content/png_logo_vector/git-icon.png',
+  },
+  {
+    skill_name: 'Heroku',
+    icon_url: 'https://manzanit0.gallerycdn.vsassets.io/extensions/manzanit0/heroku-vscode/0.3.0/1590949018168/Microsoft.VisualStudio.Services.Icons.Default',
+  },
+]
