80c1aa436dd9e26a0107b0a92b72792e257339c0
Min Marn Oo <min.marnoo@lucence.com>
2022-06-04 09:52:47 +0800

    placholder for work

--- src/components/Content/Work/Work.jsx
+++ src/components/Content/Work/Work.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
-import { Tabs } from 'antd'
-const { TabPane } = Tabs
+
 
 export default class Work extends Component {
   render() {
     return (
-      <Tabs>
-        <TabPane tab={"ASDFASDFSDFSDF"} key="1">asdf</TabPane>
-        <TabPane tab={"ASDFASDFSDFSDF"} key="2">asdf</TabPane>
-        <TabPane tab={"ASDFASDFSDFSDF"} key="3">asdf</TabPane>
-      </Tabs>
+      <div>
+        Placeholder for Work
+      </div>
     )
   }
 }
