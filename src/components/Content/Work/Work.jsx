61473d0658342702a0be4300505b8f0db6f62849
Min Marn Oo <min.marnoo@lucence.com>
2022-06-06 10:57:19 +0800

    working on work tab

--- src/components/Content/Work/Work.jsx
+++ src/components/Content/Work/Work.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-
+import {
+  ToolOutlined
+} from '@ant-design/icons'
+import { work_data } from './work_data'
+import './Work.css'
+import { CustomTimeline } from '../common/Timeline'
 
 export default class Work extends Component {
   render() {
     return (
-      <div>
-        Placeholder for Work
+      <div className="work-main-div">
+        <CustomTimeline title="Work History" icon={<ToolOutlined />} data={work_data}/>
       </div>
     )
   }
---
+++ src/components/Content/Work/work_data.jsx
@@ -0,0 +1,42 @@
+
+export const work_data = [
+  {
+    date: 'Jun 2021 - Current',
+    title: 'Software Engineer | Lucence ',
+    subtitle: (
+      <ul>
+        <li>Develop and maintain frontend applications using React, Mobx and Ant Design.</li>
+        <li>Build RESTful APIs (using API Gateway, S3, Lambda, DynamoDB and MySQL) that serves data to the frontend</li>
+        <li>Automate report generation using Google AppScript</li>
+        <li>Documentation of business workflow, system architecture and knowledge base.</li>
+        <li>Setup automatic chat bot notifications on Slack using Webhooks</li>
+      </ul>
+    ),
+  },
+  {
+    date: 'Jan 2019 - Jun 2021',
+    title: 'IT Field Service Technologist | Micron Technology, Inc.',
+    subtitle: (
+      <ul>
+        <li>Configure in house software and hardware repairs</li>
+        <li>Server preventive maintenance (disk defragmentation, administration of security and service patches)</li>
+        <li>Perform system recovery based on incident calls and under guidance from established documents</li>
+        <li>Manage inventory and IT equipments</li>
+      </ul>
+    )
+  },
+  {
+    date: 'Jun 2015 - Jan 2019',
+    title: 'Technical Specialist | Hewlett Packard Inc. ',
+    subtitle: (
+      <ul>
+        <li>Troubleshoot problems in non functioning equipment.</li>
+        <li>Dismantled, adjusted, repaired and assembled equipment according to plans, blueprints, operating or repair manuals, and/or rough sketches or drawings.</li>
+        <li>Performed equipment modification as directed by test/manufacturing engineers.</li>
+        <li>Assisted Engineers by operating the machinery at specific settings and providing data.</li>
+        <li>Liaised with vendors regarding replacement orders and tooling modifications.</li>
+      </ul>
+    )
+  },
+]
+
