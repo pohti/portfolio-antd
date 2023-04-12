76ebc9f86f6db2ff4817dff7026141381771ca14
Min Marn Oo <minmarnoo@gmail.com>
2023-04-12 10:36:27 +0800

    updating work

--- src/components/Content/Work/work_data.jsx
+++ src/components/Content/Work/work_data.jsx
@@ -1,7 +1,21 @@
 
 export const work_data = [
   {
-    date: 'Jun 2021 - Current',
+    date: 'Sep 2022 - Current',
+    title: 'Cloud Software Developer',
+    subtitle: 'dormakaba',
+    description: (
+      <ul>
+        <li>Implement robust and scalable pipelines to process and analyze high bandwidth IoT data.</li>
+        <li>Work with time series data in Python, MongoDB and other AWS services to derive meaningful data for stakeholders.</li>
+        <li>Translate design documents into actual code and products.</li>
+        <li>Documentation of system architecture.</li>
+        <li>CICD of infrastructure using Terraform.</li>
+      </ul>
+    ),
+  },
+  {
+    date: 'Jun 2021 - Sep 2022',
     title: 'Software Engineer',
     subtitle: 'Lucence',
     description: (
@@ -18,28 +32,28 @@ export const work_data = [
     date: 'Jan 2019 - Jun 2021',
     title: 'IT Field Service Technologist',
     subtitle: 'Micron Technology, Inc.',
-    description: (
-      <ul>
-        <li>Configure in house software and hardware repairs</li>
-        <li>Server preventive maintenance (disk defragmentation, administration of security and service patches)</li>
-        <li>Perform system recovery based on incident calls and under guidance from established documents</li>
-        <li>Manage inventory and IT equipments</li>
-      </ul>
-    )
+    // description: (
+    //   <ul>
+    //     <li>Configure in house software and hardware repairs</li>
+    //     <li>Server preventive maintenance (disk defragmentation, administration of security and service patches)</li>
+    //     <li>Perform system recovery based on incident calls and under guidance from established documents</li>
+    //     <li>Manage inventory and IT equipments</li>
+    //   </ul>
+    // )
   },
   {
     date: 'Jun 2015 - Jan 2019',
     title: 'Technical Specialist',
     subtitle: 'Hewlett Packard Inc.',
-    description: (
-      <ul>
-        <li>Troubleshoot problems in non functioning equipment.</li>
-        <li>Dismantled, adjusted, repaired and assembled equipment according to plans, blueprints, operating or repair manuals, and/or rough sketches or drawings.</li>
-        <li>Performed equipment modification as directed by test/manufacturing engineers.</li>
-        <li>Assisted Engineers by operating the machinery at specific settings and providing data.</li>
-        <li>Liaised with vendors regarding replacement orders and tooling modifications.</li>
-      </ul>
-    )
+    // description: (
+    //   <ul>
+    //     <li>Troubleshoot problems in non functioning equipment.</li>
+    //     <li>Dismantled, adjusted, repaired and assembled equipment according to plans, blueprints, operating or repair manuals, and/or rough sketches or drawings.</li>
+    //     <li>Performed equipment modification as directed by test/manufacturing engineers.</li>
+    //     <li>Assisted Engineers by operating the machinery at specific settings and providing data.</li>
+    //     <li>Liaised with vendors regarding replacement orders and tooling modifications.</li>
+    //   </ul>
+    // )
   },
 ]
 
